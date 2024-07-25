d7d67cb41f3b747a40a8e135bdb29134d48b9051
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-25 14:59:39 -0300

    Aula 02.03 - Removendo e substituindo elementos em arrays

---
+++ Curso 02/05-splice.js
@@ -0,0 +1,16 @@
+const students = [
+    'João V.',
+    'Maria J.',
+    'Julia B.',
+    'Ana C.',
+    'Fábio A.',
+    'Renan C.',
+    'Patrick R.',
+    'Karen S.',
+    'Luan C.',
+    'Wellington C.'
+];
+
+students.splice(1, 2, 'Rodrigo S.', 'José C.');
+
+console.log(students);
