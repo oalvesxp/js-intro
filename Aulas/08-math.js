5129c58906ddf0bea73b995c50e26983283653d0
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-25 11:23:35 -0300

    Aula 04.02 - Operadores Aritméticos

---
+++ Aulas/08-math.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 2;
+
+console.log(`${a} + ${b} = ${a + b}`);      /** soma */
+console.log(`${a} - ${b} = ${a - b}`);      /** subtração */
+console.log(`${a} * ${b} = ${a * b}`);      /** multiplicação */
+console.log(`${a} / ${b} = ${a / b}`);      /** divisão */
+console.log(`${a} % ${b} = ${a % b}`);      /** resto */
+console.log(`${a} ^ ${b} = ${a ** b}`);     /** Exponencial */
