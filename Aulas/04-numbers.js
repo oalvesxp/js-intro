55b1eb29df38af945ed6821e7be15851f07c7187
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-25 10:30:51 -0300

    Aula 03.02 - Números

---
+++ Aulas/04-numbers.js
@@ -0,0 +1,8 @@
+const rate01 = 8.2;
+const rate02 = 7.5;
+const rate03 = 4.8;
+const rate04 = Number.parseInt('6');
+
+const avg = (rate01 + rate02 + rate03 + rate04) / 4; 
+
+console.log('A média de notas é: ' + avg.toFixed(2));
