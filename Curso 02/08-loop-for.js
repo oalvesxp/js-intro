5ba74d3944cb1928f0d537ab79c3112495114cba
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-25 16:24:07 -0300

    Aula 03.09 - Looping com FOR

---
+++ Curso 02/08-loop-for.js
@@ -0,0 +1,13 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < num.length; i++) {
+    console.log(num[i]);
+}
+
+console.log('');  /** linha em branco */
+
+const alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+for (let i = 0; i < alpha.length; i++) {
+    console.log(alpha[i]);
+}
