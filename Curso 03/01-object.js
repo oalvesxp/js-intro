bae467c0714f0afe1ff3ff6cb968018c7ac646cd
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-26 14:42:42 -0300

    Aula 01.04 - Objetos em JS

---
+++ Curso 03/01-object.js
@@ -0,0 +1,9 @@
+const objStudent = {
+    name: 'Luan Cardoso',
+    age: 23,
+    cpf: '74514512553',
+    class: 'JavaScript'
+};
+
+console.log(objStudent);
+console.log(objStudent.cpf.substring(0,3));
