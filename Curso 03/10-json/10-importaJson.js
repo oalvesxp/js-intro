ae00cb4900b7b377c7dd9f1e47bc4350f998b728
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-29 20:35:42 -0300

    Aula 04.05 - Objetos JSON

---
+++ Curso 03/10-json/10-importaJson.js
@@ -0,0 +1,8 @@
+// import student from './10-student.json';
+const student = require('./10-student.json');
+
+console.log(student);
+console.log(typeof student);
+
+const keys = Object.keys(student);
+console.log(keys);
