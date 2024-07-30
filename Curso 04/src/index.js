8ae361bbb011036cfdda115972bc968a6828648e
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-30 10:47:40 -0300

    Aula 01.05 - Lendo o arquivo de texto

--- Curso 04/src/index.js
+++ Curso 04/src/index.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+
 const dir = process.argv;
 const link = dir[2];
 
 /** node '.\Curso 04\src\index.js' '.\Curso 04\files\texto-web.txt' */
+/** lendo arquivo */
+fs.readFile(link, 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+
 console.log(link);
