9b1f3ce90bd7a6e5e7c56488557f56d419c3c434
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-30 14:46:02 -0300

    Aula 02.02 - Listando palavras duplicadas

--- Curso 04/src/index.js
+++ Curso 04/src/index.js
@@ -3,11 +3,23 @@ const fs = require('fs');
 const dir = process.argv;
 const link = dir[2];
 
+/** Criando um array de palavras */
+/** Contar as ocorrências */
+/** Montar um objeto com um resultado */
+function wordsDoble(text) {
+    const listWords = text.split(' ');
+    const result = {};
+
+    listWords.forEach(word => {
+        result[word] = (result[word] || 0) + 1;
+    });
+
+    console.log(result);
+}
+
 /** node '.\Curso 04\src\index.js' '.\Curso 04\files\texto-web.txt' */
 /** lendo arquivo */
 fs.readFile(link, 'utf-8', (err, data) => {
     if (err) throw err;
-    console.log(data);
+    wordsDoble(data);
 });
-
-console.log(link);
