5c7f7eb00395fb3b20acf3d603ede6673008ce06
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-08-06 09:39:09 -0300

    Aula 03.02 - Estrutura de classe

---
+++ Curso 05/04-User.js
@@ -0,0 +1,35 @@
+class User {
+    constructor(
+        name
+        , email 
+        , birthdate
+        , role
+        , active = true
+    ) {
+        this.name       = name
+        this.email      = email
+        this.birthdate  = birthdate
+        this.role       = role || 'student'
+        this.active     = active
+    }
+
+    getInfo() {
+        return `
+            Nome:   ${this.name}
+            E-mail: ${this.email}
+            Nasc.:  ${this.birthdate}
+            Grupo:  ${this.role}
+            Ativo:  ${this.active}
+        `;
+    }
+}
+
+const user = new User(
+    'Osvaldo A. Neto'
+    , 'oalves.neto@outlook.com'
+    , '1997-02-26'
+);
+
+console.log(user);
+console.log(user.getInfo());
+console.log(User.prototype.isPrototypeOf(user));
